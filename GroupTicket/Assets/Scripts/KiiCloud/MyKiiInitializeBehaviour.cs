686973a782e023c8f47ebb24458e7222df043c22
hide suga <sugaharaaa@gmail.com>
2015-03-08 13:53:36 +0900

    KiiCloudSDK追加

---
+++ GroupTicket/Assets/Scripts/KiiCloud/MyKiiInitializeBehaviour.cs
@@ -0,0 +1,11 @@
+﻿
+
+public class MyKiiInitializeBehaviour : KiiInitializeBehaviour
+{
+	public override void Awake ()
+	{
+		base.Awake ();
+		// KiiCloudSDK has been initialized.
+		// You can call KiiCloud APIs.
+	}
+}
